4301b0a5744aec004d1574c6155481b3d93d794f
Vjeko <vjeko.pavicin@gmail.com>
2024-12-29 16:38:36 +0100

    Refactor resource handling and minor UI improvements
    Streamlined resource usage by removing unnecessary signal setup in `UserStore` and optimized data access patterns. Adjusted hover state styling in `Store` for better user experience and visual consistency.

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -12,10 +12,10 @@ const HeartIcon = ({ isFilled }: { isFilled: () => boolean }) => {
       viewBox="0 0 24 24"
       class="h-5 w-5 transition-all duration-200 ease-in-out cursor-pointer transform hover:scale-110" // Add hover scale effect
       fill={isFilled() ? 'red' : 'white'}
-      stroke={isFilled() ? 'red' : 'black'}
+      stroke={isFilled() ? 'red' : 'gray'}
       stroke-width="1.5"
       classList={{
-        'hover:stroke-black': true,
+        'hover:stroke-black': !isFilled(),
       }}
     >
       <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
--- src/pages/account/UserStore.tsx
+++ src/pages/account/UserStore.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createResource, createSignal } from 'solid-js'
+import { createResource, createSignal } from 'solid-js'
 import { useParams } from '@solidjs/router'
 import { $fetch, FetchData } from '../../utils/fetch'
 import { Hero } from '../../layout/Hero'
@@ -28,8 +28,7 @@ const fetchProducts = async () => {
 }
 
 export const UserStore = () => {
-  const [store, setStore] = createSignal<Store | null>(null)
-  const [data] = createResource<FetchData<Store>>(fetchData)
+  const [store] = createResource<FetchData<Store>>(fetchData)
 
   const [products, { refetch }] =
     createResource<FetchData<Product[]>>(fetchProducts)
@@ -41,16 +40,10 @@ export const UserStore = () => {
     setPresentProductForm(false)
   }
 
-  createEffect(() => {
-    if (data()) {
-      setStore(data().data)
-    }
-  })
-
   return (
     <div>
-      {store()?.name && (
-        <Hero name={store()?.name} image={store()?.media[0].url} />
+      {store()?.data.name && (
+        <Hero name={store().data.name} image={store().data.media[0].url} />
       )}
       <Container>
         <section class="bg-white py-8">
@@ -101,35 +94,35 @@ export const UserStore = () => {
           isOpen={presentProductForm()}
           onClose={() => setPresentProductForm(false)}
         >
-          {store() && <ProductForm store={store()} onClose={onModalClose} />}
+          {store() && (
+            <ProductForm store={store().data} onClose={onModalClose} />
+          )}
         </Modal>
 
         <section class="bg-white py-8">
           <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
-            {products() &&
-              products().data.length &&
-              products().data.map((product) => (
-                <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-                  <a href="#">
-                    <img
-                      class="hover:grow hover:shadow-lg"
-                      src={product.image[0]}
-                      alt="product image"
-                    />
-                    <div class="pt-3 flex items-center justify-between">
-                      <p class="">{product.name}</p>
-                      <svg
-                        class="h-6 w-6 fill-current text-gray-500 hover:text-black"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
-                      </svg>
-                    </div>
-                    <p class="pt-1 text-gray-900">£9.99</p>
-                  </a>
-                </div>
-              ))}
+            {products()?.data?.map((product) => (
+              <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
+                <a href="#">
+                  <img
+                    class="hover:grow hover:shadow-lg"
+                    src={product.image[0]}
+                    alt="product image"
+                  />
+                  <div class="pt-3 flex items-center justify-between">
+                    <p class="">{product.name}</p>
+                    <svg
+                      class="h-6 w-6 fill-current text-gray-500 hover:text-black"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
+                    </svg>
+                  </div>
+                  <p class="pt-1 text-gray-900">£9.99</p>
+                </a>
+              </div>
+            ))}
           </div>
           <section class="bg-white py-8">
             <div class="container py-8 px-6 mx-auto">
@@ -155,7 +148,7 @@ export const UserStore = () => {
 
               <p class="mt-8 mb-8"></p>
 
-              <p class="mb-8">{store()?.description}</p>
+              <p class="mb-8">{store()?.data.description}</p>
             </div>
           </section>
         </section>
