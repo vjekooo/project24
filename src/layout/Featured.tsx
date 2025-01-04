f08e8850d453536ba3461c0effe90b7496c35590
Vjeko <vjeko.pavicin@gmail.com>
2025-01-04 15:05:11 +0100

    Refactor Featured component to update layout structure.
    Removed outer anchor wrapping and adjusted layout to improve structure and readability. Added a standalone "visit store" link with updated styling for clarity. These changes enhance the component's design and maintainability.

--- src/layout/Featured.tsx
+++ src/layout/Featured.tsx
@@ -7,20 +7,23 @@ interface Props {
 
 export const Featured = ({ store }: Props) => {
   return (
-    <a href={`/store/${store.id}`}>
-      <div
-        class="w-full mx-auto flex items-start p-12 md:pt-0 bg-cover bg-right relative mb-52"
-        style={`max-width:1600px; height: 32rem; background-image: url('${store.media[0].imageUrl}');`}
-      >
-        <div class="flex flex-col justify-center p-6 tracking-wide bg-white opacity-90">
-          <Stack gap={3}>
-            <h1 class="text-black h1">{store.name}</h1>
-          </Stack>
-        </div>
-        <div class="absolute sm mx-12 sm:mx-0 sm:w-80 tracking-wide bg-white top-52 sm:top-44 shadow-lg right-0 sm:right-24 p-5">
-          {store.description}
-        </div>
+    <div
+      class="w-full mx-auto flex items-start p-12 md:pt-0 bg-cover bg-right relative mb-52"
+      style={`max-width:1600px; height: 32rem; background-image: url('${store.media[0].imageUrl}');`}
+    >
+      <div class="flex flex-col justify-center p-6 tracking-wide bg-white opacity-90">
+        <Stack gap={3}>
+          <h1 class="text-black h1">{store.name}</h1>
+        </Stack>
       </div>
-    </a>
+      <div class="absolute sm mx-12 sm:mx-0 sm:w-80 tracking-wide bg-white top-52 sm:top-44 shadow-lg right-0 sm:right-24 p-5">
+        <Stack gap={3}>
+          {store.description}
+          <a href={`/store/${store.id}`} class="underline">
+            visit store
+          </a>
+        </Stack>
+      </div>
+    </div>
   )
 }
