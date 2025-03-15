37191d540841e9e72e57f7ab446d8aba12a877be
Vjeko <vjeko.pavicin@gmail.com>
2025-03-15 11:06:19 +0100

    Enable Solid DevTools and adjust server configuration
    Solid DevTools is now included in the plugins array to facilitate debugging. The server configuration was updated to allow external access (`host: true`) and disable auto-opening the browser (`open: false`). These changes improve development flexibility and

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
-// import devtools from 'solid-devtools/vite';
+import devtools from 'solid-devtools/vite'
 
 export default defineConfig({
-  plugins: [
-    /*
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-  ],
+  plugins: [devtools(), solidPlugin()],
   server: {
+    host: true,
     port: 3000,
-    open: true,
+    open: false,
   },
   build: {
     target: 'esnext',
