4cf8209415b414bf54dfb3f83efe064557d0a825
Vjeko <vjeko.pavicin@gmail.com>
2025-03-15 11:08:48 +0100

    Disable Solid devtools in Vite configuration
    Commented out the Solid devtools plugin to exclude it from the build process. This change simplifies the plugin setup and likely targets a production environment or a scenario where devtools are not required.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
-import devtools from 'solid-devtools/vite'
+// import devtools from 'solid-devtools/vite'
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin()],
+  plugins: [
+    // devtools(),
+    solidPlugin(),
+  ],
   server: {
     host: true,
     port: 3000,
